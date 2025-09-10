a662b940f5196ed138862706dafc28e0e0cb1920
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2025-09-11 01:28:42 +0530

    Create Counting Frequencies of Array Elements.cpp

---
+++ Counting Frequencies of Array Elements.cpp
@@ -0,0 +1,31 @@
+//output :- 10 3 5 2 15 1   Time Complexity: O(N*N)    Space Complexity:  O(N)
+#include<iostream>
+using namepace std;
+void countFreq(int arr[], int n)
+{
+    vector < bool > visited(n, false);
+
+    for (int i = 0; i < n; i++) {
+
+        // Skip this element if already processed
+        if (visited[i] == true)
+            continue;
+
+        // Count frequency
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                visited[j] = true;
+                count++;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
+
+int main()
+{
+    int arr[] = {10,5,10,15,10,5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    countFreq(arr, n);
+    return 0;
