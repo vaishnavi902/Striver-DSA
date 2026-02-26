dea910130a5a1435d0e5e6dee3d6797a4bc5b733
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-02-26 17:25:40 +0530

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,34 +1,33 @@
-//Given an array of integers called nums, sort the array in non-decreasing order using the insertion sort algorithm and return the sorted array.
-
 #include<iostream>
 using namespace std;
 
-void InsertionSort(int arr[], int n){
-    for(int i=1; i<n ; i++){
-        int temp = arr[i];
-        int j = i-1;
-    
-        while( j>=0 && arr[j]> temp){
-            arr[j+1] = arr[j];
+void insertion_sort(int arr[], int n ,int i){
+    for(int i=0;i<n;i++){
+   
+        int j = i;
+        while(j>0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
             j--;
         }
-        arr[j+1] = temp;
     }
 };
-
 int main(){
     int arr[] = {13, 46, 24, 52, 20, 9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-     cout << "Original Array:\n";
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout<<"Before Using Insertion Sort: "<<endl;
+    for(int i=0;i<n;i++){
+        cout<< arr[i] << " ";
     }
-
-    InsertionSort(arr, n);
-
-    cout << "\nAfter Applying Insertion Sort:\n";
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
+    cout<<endl;
+    
+    insertion_sort(arr , n, 0);
+    cout<<"After Using Insertion Sort: "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
-return 0;    
+    cout<<endl;
+    return 0;
 }
