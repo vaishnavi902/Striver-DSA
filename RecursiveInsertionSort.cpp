3bf65926d1d1d38c409f04c0cc736b5deb39cb87
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-02-26 17:16:32 +0530

    Add recursive insertion sort implementation
    Implement recursive insertion sort algorithm to sort an array.

---
+++ RecursiveInsertionSort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void insertion_sort(int arr[], int n ,int i){
+    if(i == n){
+        return ;
+    }
+    int j = i;
+    while(j>0 && arr[j-1] > arr[j]){
+        int temp = arr[j-1];
+        arr[j-1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+    insertion_sort(arr,n, i+1);
+};
+int main(){
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout<<"Before Using Insertion Sort: "<<endl;
+    for(int i=0;i<n;i++){
+        cout<< arr[i] << " ";
+    }
+    cout<<endl;
+    
+    insertion_sort(arr , n, 0);
+    cout<<"After Using Insertion Sort: "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
