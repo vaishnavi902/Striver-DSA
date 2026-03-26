b9efd032afbb2c385a3abd7efe823961db814fb4
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-03-26 13:46:23 +0530

    Create Sort Colors.cpp

---
+++ Arrays/Sort Colors.cpp
@@ -0,0 +1,26 @@
+/*Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]*/
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0 , mid = 0;
+        int high = nums.size()-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid] , nums[low]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid] , nums[high]);
+                high--;
+            }
+        }
+    }
+};
