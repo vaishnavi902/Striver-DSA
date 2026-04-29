b8728b020a102ce275499a9b08afb6fc8a33becc
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-04-29 21:34:51 +0530

    Create 6.cpp

---
+++ Patterns/6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n = 5;
+    for(int i =n;i>0;i--){
+        for(int j=1;j<=i;j++){
+        cout << j;
+    }
+    cout<<endl;
+}
+return 0;
+}
+/*Output:-
+12345
+1234
+123
+12
+1
+*/
