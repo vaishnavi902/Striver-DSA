0813d48d8ccc5963bec7bfdb9b68af3ab87aa885
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-05-01 20:40:05 +0530

    Create 9.cpp

---
+++ Patterns/9.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n =5;
+    for(int i = 1;i<= n ;i++){
+        for(int k =n;k>i;k--){
+            cout<<" ";
+        }
+        for(int j = 1; j<=(2*i-1); j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n; i>=1 ; i--){
+        for(int k = 0;k<n-i;k++){
+            cout<< " " ;
+        }
+        for(int j =1; j<=(2*i-1);j++){
+        cout << "*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+/*Output:
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
