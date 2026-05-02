0b8d6fdd96136eb3d120277213718aa52d3b72c4
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-05-02 10:32:10 +0530

    Create 14.cpp

---
+++ Patterns/14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n =5;
+    for (int i = 1; i <= n; i++) {
+        for (char ch = 'A'; ch < 'A' + i; ch++) {
+            cout << ch;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*Output:
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
