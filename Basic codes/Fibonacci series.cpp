5e1a8bf2e17509e8fe1907fe7889895ac489f3f7
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-07-14 12:02:29 +0530

    Create Fibonacci series.cpp

---
+++ Basic codes/Fibonacci series.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a = 0, b = 1;
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+}
