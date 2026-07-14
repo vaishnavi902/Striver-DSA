98f916c9010d3ae55fdd168e7344a8cab163e499
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-07-14 12:03:30 +0530

    Update Fibonacci series.cpp

--- Basic codes/Fibonacci series.cpp
+++ Basic codes/Fibonacci series.cpp
@@ -14,3 +14,45 @@ int main() {
         b = c;
     }
 }
+
+
+
+
+
+------2nd method---------
+
+#include <iostream>
+using namespace std;
+
+class Fibonacci {
+public:
+    void printSeries(int n) {
+        int start = 0;
+        int next = 1;
+        int ans; // Declared the answer variable used in your notes
+
+        if (start == 0 && next == 0) {
+            return;
+        }
+
+        // Loops 'n' times to generate the sequence
+        while (n > 0) {
+            ans = start + next;
+            start = next;
+            next = ans;
+            
+            cout << start << endl;
+            n--;
+        }
+    }
+};
+
+int main() {
+    Fibonacci f;
+    int terms = 5; // Change this number to print more or fewer terms
+    
+    cout << "Fibonacci Series:" << endl;
+    f.printSeries(terms);
+    
+    return 0;
+}
