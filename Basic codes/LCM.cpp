fc3c0ca560885adade2b79cf89544199df252d28
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-07-15 18:25:45 +0530

    Create LCM.cpp

---
+++ Basic codes/LCM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << (a * b) / gcd(a, b);
+}
